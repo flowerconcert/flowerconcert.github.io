2e29c66a2def77bb9431442c8be1d48d09bdf229
EverSinceWWI <32314108+EverSinceWWI@users.noreply.github.com>
2020-05-26 01:44:26 +0200

    alpha test version

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var song = document.getElementById('song');
+var isPlaying = true;
+
+$(".vol").click(function(){
+  $(".bi-volume-down").toggleClass("d-none");
+  $(".bi-volume-mute").toggleClass("d-none");
+  song.muted = !song.muted;
+});
+
+
+  $(".play").click(function(){
+    $(".bi-pause").toggleClass("d-none");
+    $(".bi-play").toggleClass("d-none");
+    if (isPlaying) {
+      song.pause();
+    } else {
+      song.play();
+    }
+    isPlaying = !isPlaying
+  });
+
+  song.oncanplaythrough = function() {
+    $(".loading").addClass("d-none");
+    $(".play").removeClass("d-none");
+    $(".vol").removeClass("d-none");
+};
